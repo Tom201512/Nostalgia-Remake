585f18998b1921524573ba0f1804628b3f445169
Tom201512 <166293030+Tom201512@users.noreply.github.com>
2025-10-19 23:42:48 +0900

    一部UI画面、表示を修正

--- Assets/Scripts/Option/ScreenUI/ProbabilityDataUI.cs
+++ Assets/Scripts/Option/ScreenUI/ProbabilityDataUI.cs
@@ -26,7 +26,7 @@ namespace ReelSpinGame_Option.MenuContent
             }
             else
             {
-                data += "1/---\n";
+                data += "1/---" + "\n";
             }
 
             data += "ボーナスゲーム確率 BONUS GAME: ";
@@ -37,76 +37,75 @@ namespace ReelSpinGame_Option.MenuContent
             }
             else
             {
-                data += "1/---\n" + "\n";
+                data += "1/---" + "\n" + "\n";
             }
 
             // 小役確率
-            data += "通常時小役確率 Normal mode probability:" + "\n" + "\n";
-            data += "ベル Bell: ";
+            data += "ベル Bell: " + "\n";
             data += " 確率 Probability: ";
 
             if (player.PlayerAnalyticsData.NormalBellHitCount > 0)
             {
                 float probability = (float)player.TotalGames / player.PlayerAnalyticsData.NormalBellHitCount;
-                data += "1/" + probability.ToString("F3");
+                data += "1/" + probability.ToString("F3") + "\t\t\t\t";
             }
             else
             {
-                data += "1/---";
+                data += "1/---" + "\t\t\t\t";
             }
 
-            data += " 成立 Hit: " + player.PlayerAnalyticsData.NormalBellHitCount;
+            data += " 成立 Hit: " + player.PlayerAnalyticsData.NormalBellHitCount + "\t\t\t\t";
             data += " 入賞 Line Up: " + player.PlayerAnalyticsData.NormalBellLineUpCount + "\n";
 
 
-            data += "スイカ Melon: ";
+            data += "スイカ Melon: " + "\n";
             data += " 確率 Probability: ";
 
             if (player.PlayerAnalyticsData.NormalMelonHitCount > 0)
             {
                 float probability = (float)player.TotalGames / player.PlayerAnalyticsData.NormalMelonHitCount;
-                data += "1/" + probability.ToString("F3");
+                data += "1/" + probability.ToString("F3") + "\t\t\t\t";
             }
             else
             {
-                data += "1/---";
+                data += "1/---" + "\t\t\t\t";
             }
 
-            data += " 成立 Hit: " + player.PlayerAnalyticsData.NormalMelonHitCount;
+            data += " 成立 Hit: " + player.PlayerAnalyticsData.NormalMelonHitCount + "\t\t\t\t";
             data += " 入賞 Line Up: " + player.PlayerAnalyticsData.NormalMelonLineUpCount + "\n";
 
 
-            data += "2枚チェリー Cherry2: ";
+            data += "2枚チェリー Cherry2: " + "\n";
             data += " 確率 Probability: ";
 
             if (player.PlayerAnalyticsData.NormalCherry2HitCount > 0)
             {
                 float probability = (float)player.TotalGames / player.PlayerAnalyticsData.NormalCherry2HitCount;
-                data += "1/" + probability.ToString("F3");
+                data += "1/" + probability.ToString("F3") + "\t\t\t\t";
             }
             else
             {
-                data += "1/---";
+                data += "1/---" + "\t\t\t\t";
             }
 
-            data += " 成立 Hit: " + player.PlayerAnalyticsData.NormalCherry2HitCount;
+            data += " 成立 Hit: " + player.PlayerAnalyticsData.NormalCherry2HitCount + "\t\t\t\t";
             data += " 入賞 Line Up: " + player.PlayerAnalyticsData.NormalCherry2LineUpCount + "\n";
 
 
-            data += "4枚チェリー Cherry4: ";
+            data += "4枚チェリー Cherry4: " + "\n";
             data += " 確率 Probability: ";
 
             if (player.PlayerAnalyticsData.NormalCherry4HitCount > 0)
             {
                 float probability = (float)player.TotalGames / player.PlayerAnalyticsData.NormalCherry4HitCount;
-                data += "1/" + probability.ToString("F3");
+                data += "1/" + probability.ToString("F3") + "\t\t\t\t";
             }
             else
             {
-                data += "1/---";
+                data += "1/---" + "\t\t\t\t";
             }
 
-            data += " 成立 Hit: " + player.PlayerAnalyticsData.NormalCherry4HitCount;
+            data += " 成立 Hit: " + player.PlayerAnalyticsData.NormalCherry4HitCount + "\t\t\t\t";
             data += " 入賞 Line Up: " + player.PlayerAnalyticsData.NormalCherry4LineUpCount + "\n";
 
             textUI.text = data;
