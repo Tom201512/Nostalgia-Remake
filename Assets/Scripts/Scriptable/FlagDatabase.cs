44c9c20d32890df26b1b68b399676937f207407b
Tom201512 <166293030+Tom201512@users.noreply.github.com>
2026-03-03 22:54:32 +0900

    フラグデータベースファイルを更新

--- Assets/Scripts/Scriptable/FlagDatabase.cs
+++ Assets/Scripts/Scriptable/FlagDatabase.cs
@@ -82,36 +82,6 @@ namespace ReelSpinGame_Datas
             get => jacTable;
             set => jacTable = value;
         }
-
-        //Deleted
-        [SerializeField] FlagDataSets normalATable;         // 通常時A(低確率)
-        [SerializeField] FlagDataSets normalBTable;         // 通常時B(高確率)
-        [SerializeField] FlagDataSets bigTable;             // 小役ゲーム中
-        [SerializeField] float jacNoneProbability;          // JAC時のはずれ
-
-        public FlagDataSets NormalATable
-        {
-            get => normalATable;
-            set => normalATable = value;
-        }
-
-        public FlagDataSets NormalBTable
-        {
-            get => normalBTable;
-            set => normalBTable = value;
-        }
-
-        public FlagDataSets BigTable
-        {
-            get => bigTable;
-            set => bigTable = value;
-        }
-
-        public float JacNonePoss
-        {
-            get => jacNoneProbability;
-            set => jacNoneProbability = value;
-        }
     }
 
     // 各テーブル、ベット枚数ごとのフラグデータ
@@ -154,8 +124,8 @@ namespace ReelSpinGame_Datas
         {
             flagCountBySetting = new List<int>();
             string[] values = loadedData.ReadLine().Split(',');
-            Debug.Log("C:" + values.Length);
             int index = 0;
+
             foreach (string s in values)
             {
                 // 最初の数値はフラグIDを登録する
@@ -167,50 +137,7 @@ namespace ReelSpinGame_Datas
                 {
                     flagCountBySetting.Add(Convert.ToInt32(s));
                 }
-
-                Debug.Log(s);
                 index += 1;
-            }
-        }
-    }
-
-    //Deleted
-    // 設定ごとにまとめたフラグ確率
-    [Serializable]
-    public class FlagDataSets
-    {
-        [SerializeField] List<FlagDataBySetting> flagDataBySettings;
-        public List<FlagDataBySetting> FlagDataBySettings { get { return flagDataBySettings; } }
-
-        public FlagDataSets(StreamReader loadedData)
-        {
-            flagDataBySettings = new List<FlagDataBySetting>();
-            // 全ての行を読み込む
-            while (loadedData.Peek() != -1)
-            {
-                flagDataBySettings.Add(new FlagDataBySetting(loadedData));
-            }
-        }
-    }
-
-    // 設定ごとの確率
-    [Serializable]
-    public class FlagDataBySetting
-    {
-        // フラグ確率
-        [SerializeField] private float[] flagTable;
-        public float[] FlagTable { get { return flagTable; } }
-
-        public FlagDataBySetting(StreamReader loadedData)
-        {
-            string[] values = loadedData.ReadLine().Split(',');
-            string buffer = "";
-            // 配列に変換
-            flagTable = Array.ConvertAll(values, float.Parse);
-
-            foreach (float f in flagTable)
-            {
-                buffer += f + ",";
             }
         }
     }
